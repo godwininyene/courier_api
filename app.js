2a0066f91ff3ac0824ffbb09d8603c575ac9facb
godwininyene <godwinhigh2@gmail.com>
2025-05-24 03:44:52 -0700

    WIP: Use defined origin

--- app.js
+++ app.js
@@ -14,14 +14,14 @@ app.use(cookieParser())
 
 //Implement cors
 app.use(cors({
-    origin: '*',
+    origin: 'http://127.0.0.1:5500',
 //   origin: process.env.FRONTEND_URL,
   credentials: true,               // Allow credentials such as cookies
 }));
 
 app.options('*', cors({
     // origin: process.env.FRONTEND_URL,
-    origin: '*',
+    origin: 'http://127.0.0.1:5500',
     credentials: true,
 }));
 
